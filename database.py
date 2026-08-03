c05a9bab8a70b87439f17b9707136f3e4be79b47
SAIVANDANI <162975574+SAIVANDANI@users.noreply.github.com>
2026-08-03 11:46:42 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,12 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv("mysql+pymysql://avnadmin:AVNS_R7zPpzBjoL6Nr-cDFrP@mysql-dbf90ba-saivandani7-a273.k.aivencloud.com:12909/defaultdb")
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+print("DATABASE_URL:", DATABASE_URL)   # Temporary for debugging
+
+if DATABASE_URL is None:
+    raise RuntimeError("DATABASE_URL is not set")
 
 engine = create_engine(DATABASE_URL)
 
